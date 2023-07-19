6f82c20a253b8957fc47a2c0be30ee19e94ea077
[Elijah <[efrancis64@gmail.com]>
2023-07-19 12:31:09 -0400

    added gitignore and first get and post requests for the db file

--- server.js
+++ server.js
@@ -6,7 +6,30 @@ app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
-  });
+});
+
+app.get('/api/notes', (req, res) => res.status(200).json('./db/db'));
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text
+    };
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in saving note');
+  }
+});
 
 app.listen(port, () =>
   console.info(`Example app listening at http://localhost:${port} 🚀`)
